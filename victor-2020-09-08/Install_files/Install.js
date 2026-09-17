dda9d7de8b68707c17075348d34fa25c4ed10454
fengbjlab <fengbjlab@gmail.com>
2026-09-17 14:42:02 -0600

    Add perch and victor

---
+++ victor-2020-09-08/Install_files/Install.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20221209
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Install_files/InstallMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
