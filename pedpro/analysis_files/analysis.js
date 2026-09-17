cd2bb077b298ec50bf4d0a8e9fc3308ea424a431
fengbjlab <fengbjlab@gmail.com>
2026-09-17 14:51:56 -0600

    Add pedpro

---
+++ pedpro/analysis_files/analysis.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20220705
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('analysis_files/analysisMoz.css')
+detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
